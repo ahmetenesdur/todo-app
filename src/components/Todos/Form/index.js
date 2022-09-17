a862828caaa90d26d91db17ee145a6e8614e1476
Ahmet Enes Dur <ahmetenesdur@gmail.com>
2022-09-17 22:00:03 +0300

    added a warning must be at least 3 characters long

--- src/components/Todos/Form/index.js
+++ src/components/Todos/Form/index.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
 
 const initialFormValues = { input: '', completed: false }
 
@@ -13,26 +15,41 @@ function Form({ addTodos, todos }) {
         setFormValues({ ...formValues, input: e.target.value })
     }
 
+    const alert = () => {
+        if (formValues.input.length < 3 && formValues.input.length > 0) {
+            return (<Stack sx={{ width: '100%' }} spacing={2}>
+                <Alert severity="warning">
+                    Todo must be at least 3 characters long
+                </Alert>
+            </Stack>)
+        }
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault()
 
         if (formValues.input.trim() === '') return '';
+        if (formValues.input.length < 3) return '';
 
         addTodos([...todos, formValues])
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                className='new-todo'
-                placeholder='What needs to be done?'
-                autoFocus
-                autoComplete='off'
-                name='input'
-                value={formValues.input}
-                onChange={handleChange}
-            />
-        </form>
+        <div>
+            <form
+                onSubmit={handleSubmit}>
+                <input
+                    className='new-todo'
+                    placeholder='What needs to be done?'
+                    autoFocus
+                    autoComplete='off'
+                    name='input'
+                    value={formValues.input}
+                    onChange={handleChange}
+                />
+            </form>
+            {alert()}
+        </div>
     )
 }
 
